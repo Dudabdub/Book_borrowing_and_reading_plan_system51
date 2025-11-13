6573f210b3abf9379adc5010a6bdd9551ee9a733
Dudabdub <i3041532161@gmail.com>
2025-11-13 21:45:37 +0800

    设置应用密钥与数据库路径

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'booktrack.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ITEMS_PER_PAGE = 8
+ 
