9646dc6e90330d491ee24f26579f26a85f4b3e3a
Dudabdub <i3041532161@gmail.com>
2025-11-13 21:50:19 +0800

    初始化工具包目录

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+db = SQLAlchemy() 
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
